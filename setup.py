97874905e43d5552bc7512fa6974c28d6b713d86
Chinzo Dandar <chinzo.dandar@gmail.com>
2018-08-15 11:34:50 -0700

    fix configuration

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.20180815.4',
+    version='0.20180815.5',
 
     description='Irwin Lab v2.0',
     long_description='',
